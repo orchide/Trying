169b6ee31b983d551ff1a667e022396dfb326a42
Orchide Irakoze <orchideirakoze@gmail.com>
2020-03-26 17:26:13 +0200

    only finishing touches left here and there

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ M.Slider.init(slider, {
     
 })
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems, options);
+//   });
+
+const modal = document.querySelector('.modal');
+M.Modal.init(modal, {
+
+})
